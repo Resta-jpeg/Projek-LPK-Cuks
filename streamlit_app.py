763e15e432951846ac5a735c6dba1c7580eb0d9e
Resta-jpeg <resta110090@gmail.com>
2025-12-11 15:25:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 KADAR AIR WOYYYY")
-st.write(
-    "Ayo itung kadar airmu [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("KALKULATOR PERHITUNGAN KADAR AIR")
+st.title("red[*MASUK* KE DALAM]")
