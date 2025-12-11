71ddd144607099e35085b507a0d2c3957ae74a11
Resta-jpeg <resta110090@gmail.com>
2025-12-11 15:50:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("PENENTUAN BILANGAN GANJIL DAN GENAP")
 st.title(":green[MASUK KE DALAM]")
 
-number = st.number_input("Masukkan angka":, min_value=0, max_value=10000")
+number = st.number_input("Masukkan angka":, min_value=0, max_value=10000)
 if number%2==1:
     st.write("Bilangan",number,"termasuk bilangan ganjil")
 else:
